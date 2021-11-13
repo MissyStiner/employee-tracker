890925a4270d31c86d1d5aa0839e7fe144296dfc
Missy Stiner <missystiner@gmail.com>
2021-11-13 14:00:39 -0600

    mysql database connected

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,6 +8,24 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'password',
+      database: 'employees'
+    },
+    console.log('Connected to the employees database.')
+  );
+
+
+db.query(`SELECT * FROM employee`, (err, rows) => {
+    console.log(rows);
+  });
+  
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
