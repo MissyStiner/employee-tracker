7ef272241128d2d3d18c02a0e22d4f374b7e1c40
Missy Stiner <missystiner@gmail.com>
2021-11-13 23:17:07 -0600

    user input attempted

---
+++ index.js
@@ -0,0 +1,71 @@
+// const fs = require('fs');
+// const path = require('path');
+// const inquirer = require('inquirer');
+
+// // TODO: Create an array of questions for user input
+// const questions = [
+//     {
+//         type: 'input',
+//         name: 'github',
+//         message: 'What is your GitHub username?'
+//     },
+//     {
+//         type: 'input',
+//         name: 'email',
+//         message: 'What is your email address?'
+//     },
+//     {
+//         type: 'input',
+//         name: 'title',
+//         message: "What is the name of your project?"
+//     },
+//     {
+//         type: 'input',
+//         name: 'description',
+//         message: 'Write your project description'
+//     },
+//     {
+//         type: 'list',
+//         name: 'license',
+//         message: 'What kind of license should your project have?',
+//         choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
+//       },
+//       {
+//         type: 'input',
+//         name: 'installation',
+//         message: 'What command should be run to install dependencies?',
+//         default: 'npm i'
+//       },
+//       {
+//         type: 'input',
+//         name: 'test',
+//         message: 'What command should be run to run tests?',
+//         default: 'npm test'
+//       },
+//       {
+//         type: 'input',
+//         name: 'usage',
+//         message: 'What does the user need to know about using the repo?'
+//       },
+//       {
+//         type: 'input',
+//         name: 'contributing',
+//         message: 'What does the user need to know about contributing to the repo?',
+//         default: 'Check out the Contributor Covenant industry standard: https://www.contributor-covenant.org/'
+//       }
+// ];
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {
+//     return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+// }
+
+// // TODO: Create a function to initialize app
+// function init() {
+//     inquirer.prompt(questions).then(inquirerResponses => {
+//         console.log('Generating README...');
+//         writeToFile('README.md', generateMarkdown({ ...inquirerResponses }));
+//     });
+// }
+
+// init();
