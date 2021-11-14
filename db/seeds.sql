c6b5bc14a5c5e2030786f3648658fd054c2af4db
Missy Stiner <missystiner@gmail.com>
2021-11-13 20:39:40 -0600

    foreign keys set up to reference data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,29 +1,17 @@
--- ## Employees
--- - 1: Paul Clark, SVP, Tech
--- - 2: Travis Pitts, Manager, Tech
--- - 3: Jessica Smithe, Intern, Tech
--- - 4: Susan Jones, SVP, MKT
--- - 5: Kelsey Bowman, JVP, MKT
--- - 6: Jason Masongill, Manager, MKT
+-- ## Departments
+-- - 1: MKT
+-- - 2: Tech
+-- - 3: BPS
+-- - 4: PM
+-- - 5: HR
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO department (name)
 VALUES
-('Paul', 'Clarke', 1, 1),
-('Travis', 'Pitts', 4, 1),
-('Jessica', 'Smithe', 7, 1),
-('Susan', 'Jones', 1, 4),
-('Kelsey', 'Bowman', 2, 4),
-('Jason', 'Masongill', 4, 4),
-('Ronald', 'Firbank', 6, 2),
-('Virginia', 'Woolf', 6, 2),
-('Piers', 'Gaveston', 6, 2),
-('Charles', 'LeRoi', 6, 6),
-('Katherine', 'Mansfield', 6, 6),
-('Dora', 'Carrington', 6, 6),
-('Edward', 'Bellamy', 6, 6),
-('Montague', 'Summers', 7, 4),
-('Octavia', 'Butler', 7, 4),
-('Unica', 'Zurn', 7, 4);
+('MKT'),
+('Tech'),
+('BPS'),
+('PM'),
+('HR');
 
 -- ## Roles
 -- - 1: SVP $90K
@@ -72,10 +60,29 @@ VALUES
 ('Specialist', 40000, 5),
 ('Intern', 30000, 5);
 
+-- ## Employees
+-- - 1: Paul Clark, SVP, Tech
+-- - 2: Travis Pitts, Manager, Tech
+-- - 3: Jessica Smithe, Intern, Tech
+-- - 4: Susan Jones, SVP, MKT
+-- - 5: Kelsey Bowman, JVP, MKT
+-- - 6: Jason Masongill, Manager, MKT
 
--- ## Departments
--- - 1: MKT
--- - 2: Tech
--- - 3: BPS
--- - 4: PM
--- - 5: HR
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('Paul', 'Clarke', 1, 1),
+('Travis', 'Pitts', 4, 1),
+('Jessica', 'Smithe', 7, 1),
+('Susan', 'Jones', 1, 4),
+('Kelsey', 'Bowman', 2, 4),
+('Jason', 'Masongill', 4, 4),
+('Ronald', 'Firbank', 6, 2),
+('Virginia', 'Woolf', 6, 2),
+('Piers', 'Gaveston', 6, 2),
+('Charles', 'LeRoi', 6, 6),
+('Katherine', 'Mansfield', 6, 6),
+('Dora', 'Carrington', 6, 6),
+('Edward', 'Bellamy', 6, 6),
+('Montague', 'Summers', 7, 4),
+('Octavia', 'Butler', 7, 4),
+('Unica', 'Zurn', 7, 4);
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
 const mysql = require('mysql2');
+const express = require('express');
 const inputCheck = require('./utils/inputCheck');
 
 const PORT = process.env.PORT || 3001;
@@ -24,7 +24,12 @@ const db = mysql.createConnection(
 
 // Get all employees
 app.get('/api/employees', (req, res) => {
-    const sql = `SELECT * FROM employee`;
+    const sql = `SELECT employee.*, role.title
+    AS title
+    FROM employee
+    LEFT JOIN role
+    ON employee.role_id = employee.id
+    `;
   
     db.query(sql, (err, rows) => {
       if (err) {
@@ -40,7 +45,12 @@ app.get('/api/employees', (req, res) => {
 
 // Get a single employee
 app.get('/api/employees/:id', (req, res) => {
-    const sql = `SELECT * FROM employee WHERE id = ?`;
+    const sql = `SELECT employee.*, role.title
+                AS title
+                FROM employee
+                LEFT JOIN role
+                ON employee.role_id = employee.id 
+                WHERE id = employee.id = ?`;
     const params = [req.params.id];
   
     db.query(sql, params, (err, row) => {
@@ -84,7 +94,6 @@ app.post('/api/employees', ({ body }, res) => {
       res.status(400).json({ error: errors });
       return;
     }
-    
     const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
     VALUES (?,?,?,?)`;
   const params = [
@@ -116,7 +125,87 @@ app.post('/api/employees', ({ body }, res) => {
 //   }
 //   console.log(result);
 // });
-  
+
+app.get('/api/roles', (req, res) => {
+  const sql = `SELECT * FROM role`;
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+
+app.get('/api/roles/:id', (req, res) => {
+  const sql = `SELECT * FROM role WHERE id = ?`;
+  const params = [req.params.id];
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
+});
+
+app.delete('/api/roles/:id', (req, res) => {
+  const sql = `DELETE FROM role WHERE id = ?`;
+  const params = [req.params.id];
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: res.message });
+      // checks if anything was deleted
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Role not found'
+      });
+    } else {
+      res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+      });
+    }
+  });
+});
+
+// Update an employees role
+app.put('/api/employee/:id', (req, res) => {
+  const errors = inputCheck(req.body, 'role_id');
+
+if (errors) {
+  res.status(400).json({ error: errors });
+  return;
+}
+  const sql = `UPDATE employee SET role_id = ? 
+               WHERE id = ?`;
+  const params = [req.body.party_id, req.params.id];
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      // check if a record was found
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Employee not found'
+      });
+    } else {
+      res.json({
+        message: 'success',
+        data: req.body,
+        changes: result.affectedRows
+      });
+    }
+  });
+});
+
+
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
